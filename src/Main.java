ee88d90e79c5c25afaa483b925164dfab69f17a5
Evgeniy Zlobina <ZEA_6590@mail.ru>
2023-05-09 13:01:36 +0300

    reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,9 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-      int st;
-      st = 13676;
-      int bonus;
-      bonus = 20;
-      double mill;
-      mill = st/bonus;
-        System.out.printf("результат = " +mill);
-
-
-
+        int st = 13676;
+        int bonus = 20;
+        double mill;
+        mill = st / bonus;
+        System.out.printf("результат = " + mill);
     }
 }
