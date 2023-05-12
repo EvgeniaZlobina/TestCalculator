7ee98ff65b9426165adadf4d78e83f19d666ee72
Evgeniy Zlobina <ZEA_6590@mail.ru>
2023-05-12 21:28:28 +0300

    the code was formatted repeatedly

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int st = 13676;// количество миль
-        int bonus = 20;
-        double mill;
-        mill = st / bonus;
-        System.out.printf("результат = " + mill);
+        int bonus = st / 20;
+        System.out.printf("результат = " + bonus);
     }
 }
