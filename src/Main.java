9b196d34d7864a150e272c077d2d71706f455e7c
Evgeniy Zlobina <ZEA_6590@mail.ru>
2023-05-12 21:21:17 +0300

    formatted code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int st = 13676;
+        int st = 13676;// количество миль
         int bonus = 20;
         double mill;
         mill = st / bonus;
