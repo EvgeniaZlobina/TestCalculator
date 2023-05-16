b32eb95bae0d4823af4e9a1e0e376e030ddde915
Evgeniy Zlobina <ZEA_6590@mail.ru>
2023-05-16 18:48:46 +0300

    Renaming a variable

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int st = 13676;// количество миль
-        int bonus = st / 20;
+        int bonus = st / 20; // вычисление бонусов
         System.out.printf("результат = " + bonus);
     }
 }
