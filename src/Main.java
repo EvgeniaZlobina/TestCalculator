a401ce41f0719928d8e411e23e7c4c9dc8b285ae
Evgeniy Zlobina <ZEA_6590@mail.ru>
2023-05-16 18:53:54 +0300

    Renaming a veriable st

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int st = 13676;// количество миль
-        int bonus = st / 20; // вычисление бонусов
+        int distance = 13676;// количество миль
+        int bonus = distance / 20; // вычисление бонусов
         System.out.printf("результат = " + bonus);
     }
 }
